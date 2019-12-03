85d2cfaccb55ff414a1a5ed0800f37bca1b0adc2
faisalprady <faisalprady@gmail.com>
2019-12-03 15:40:26 +0100

    update menu-items to navigate withRouter

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
 import "./App.css";
 import Homepage from "./pages/homepage/Homepage";
 
@@ -6,7 +7,10 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Homepage />
+        <Switch>
+          <Route exact path="/" component={Homepage}></Route>
+          <Route path="/hats" component={Homepage}></Route>
+        </Switch>
       </div>
     );
   }
--- src/components/menu-item/MenuItem.js
+++ src/components/menu-item/MenuItem.js
@@ -1,9 +1,14 @@
 import React from "react";
 import "./menu-item.styles.scss";
+import { withRouter } from "react-router-dom";
 
-const MenuItem = ({ title, imageUrl, size }) => {
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => {
   return (
-    <div className={`menu-item ${size}`}>
+    <div
+      className={`menu-item ${size}`}
+      //this gonna give you the base url that matched ('/') plus the {linkUrl} (shop/hats) with the string combined
+      onClick={() => history.push(`${match.url}${linkUrl}`)}
+    >
       <div
         className="background-image"
         style={{ backgroundImage: `url(${imageUrl}` }}
@@ -16,4 +21,4 @@ const MenuItem = ({ title, imageUrl, size }) => {
   );
 };
 
-export default MenuItem;
+export default withRouter(MenuItem);
