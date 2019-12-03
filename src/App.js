2fe2ed9618ef89c2b8ab1bbaa3882c2484d0139a
faisalprady <faisalprady@gmail.com>
2019-12-03 14:02:15 +0100

    homepage into smaller components and finished styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
-import Homepage from "./Homepage";
+import Homepage from "./pages/homepage/Homepage";
 
 class App extends Component {
   render() {
