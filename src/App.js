8575ef53c9402966796b9fae07392afa1d526902
faisalprady <faisalprady@gmail.com>
2019-12-03 13:04:13 +0100

    added files for homepage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Homepage from "./Homepage";
 
 class App extends Component {
   render() {
-    return <div className='App'>Hello World</div>;
+    return (
+      <div>
+        <Homepage />
+      </div>
+    );
   }
 }
 
