06617020c78b7d8eda1ad77bed3b5c21a3eaba81
faisalprady <faisalprady@gmail.com>
2020-01-14 12:33:03 +0100

    add dropdown cart list

--- src/components/cart-dropdown/CartDropdown.js
+++ src/components/cart-dropdown/CartDropdown.js
@@ -3,18 +3,23 @@ import React from "react";
 import "./cart-dropdown.styles.scss";
 import CustomButton from "../custom-button/CustomButton";
 import { connect } from "react-redux";
+import CartItem from "../cart-item/CartItem";
 
-const CartDropdown = cartItems => {
+const CartDropdown = ({ cartItems }) => {
   return (
     <div className="cart-dropdown">
-      <div className="cart-items"></div>
+      <div className="cart-items">
+        {cartItems.map(cartItem => (
+          <CartItem item={cartItem} key={cartItem.id} />
+        ))}
+      </div>
       <CustomButton>GO TO CHECKOUT</CustomButton>
     </div>
   );
 };
 
 const mapStateToProps = ({ cart: { cartItems } }) => ({
-  cartItems: cartItems
+  cartItems
 });
 
 export default connect(mapStateToProps)(CartDropdown);
