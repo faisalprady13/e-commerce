106e08ad9fd126e238c9e0372af676da00522d96
faisalprady <faisalprady@gmail.com>
2020-01-20 15:27:37 +0100

    Updates

---
+++ precache-manifest.caf8505650e5be2ffa3df7a995f7a507.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5e4bbcbd010d54c5f4fd",
+    "url": "/e-commerce/static/css/main.dd4b4679.chunk.css"
+  },
+  {
+    "revision": "5e4bbcbd010d54c5f4fd",
+    "url": "/e-commerce/static/js/main.3103e84f.chunk.js"
+  },
+  {
+    "revision": "290256ac1007569aecdb",
+    "url": "/e-commerce/static/js/runtime~main.8575550b.js"
+  },
+  {
+    "revision": "57deac343db801ca41ac",
+    "url": "/e-commerce/static/js/2.5ed63da5.chunk.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/e-commerce/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/e-commerce/static/media/shopping-bag.30f07071.svg"
+  },
+  {
+    "revision": "5c2135a5e549d2015a20e23533ccdb13",
+    "url": "/e-commerce/index.html"
+  }
+];
